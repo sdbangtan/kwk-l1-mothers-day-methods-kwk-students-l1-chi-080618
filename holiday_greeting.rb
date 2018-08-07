93a9fffca78b6c41f2cf6d4be7ba076a0760111d
Saumya Malhotra <ideagem1@gmail.com>
2018-08-07 16:38:34 +0000

    Done.

---
+++ holiday_greeting.rb
@@ -0,0 +1,5 @@
+def holiday_greeting(holiday="Mother's Day", nameto="Mom", namefrom="Your Favorite Child")
+  "Happy #{holiday}, #{nameto}! - From #{namefrom}"
+end
+
+holiday_greeting("Fourth of July", "Beyonce", "Jay-Z")
